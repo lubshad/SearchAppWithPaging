922029666377258bd0409955219b5fbd1b15c34d
lubshad <lubshad4u4@gmail.com>
2021-11-19 21:59:59 +0530

    basic navigaion and layouts added

---
+++ app/src/main/java/com/example/searchPaging/ui/fragment_gallery/FragmentGallery.kt
@@ -0,0 +1,7 @@
+package com.example.searchPaging.ui.fragment_gallery
+
+import androidx.fragment.app.Fragment
+import com.example.searchPaging.R
+
+class FragmentGallery: Fragment(R.layout.fragment_gallery) {
+}
