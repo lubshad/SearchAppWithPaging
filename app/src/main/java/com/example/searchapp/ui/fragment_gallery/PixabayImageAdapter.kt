0ff7ba055ddc9042d2349e08e39c08f64313535d
lubshad <lubshad4u4@gmail.com>
2021-11-23 09:40:55 +0530

    image adapter set

--- app/src/main/java/com/example/searchapp/ui/fragment_gallery/PixabayImageAdapter.kt
+++ app/src/main/java/com/example/searchapp/ui/fragment_gallery/PixabayImageAdapter.kt
@@ -1,1 +1,58 @@
+@file:Suppress("SpellCheckingInspection")
+
 package com.example.searchapp.ui.fragment_gallery
+
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.searchapp.data.models.pixabay.Hit
+import com.example.searchapp.databinding.PixabayImageItemBinding
+import com.example.searchapp.ui.fragment_gallery.PixabayImageAdapter.*
+
+
+class PixabayImageAdapter :
+    PagingDataAdapter<Hit, PixabayImageViewHolder>(PixabayDiffCallBack) {
+
+
+    inner class PixabayImageViewHolder(private val binding: PixabayImageItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Hit) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(item.webformatURL)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageViewItem)
+                textViewAuthor.text = item.user
+            }
+        }
+
+    }
+
+
+    object PixabayDiffCallBack : DiffUtil.ItemCallback<Hit>() {
+        override fun areItemsTheSame(oldItem: Hit, newItem: Hit): Boolean = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Hit, newItem: Hit): Boolean = oldItem == newItem
+
+    }
+
+    override fun onBindViewHolder(holder: PixabayImageViewHolder, position: Int) {
+        val position = holder.bindingAdapterPosition
+        if (position != RecyclerView.NO_POSITION) {
+            val item = getItem(position)
+            if (item != null) {
+                holder.bind(item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PixabayImageViewHolder {
+        val binding = PixabayImageItemBinding.bind(parent)
+        return PixabayImageViewHolder(binding)
+    }
+
+}
