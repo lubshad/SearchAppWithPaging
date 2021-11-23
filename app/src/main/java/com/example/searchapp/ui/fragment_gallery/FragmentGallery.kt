0b54be56f63f7663f24b2d43e4ea9c6ed6b3aef5
lubshad <lubshad4u4@gmail.com>
2021-11-23 10:51:16 +0530

    search implemented

--- app/src/main/java/com/example/searchapp/ui/fragment_gallery/FragmentGallery.kt
+++ app/src/main/java/com/example/searchapp/ui/fragment_gallery/FragmentGallery.kt
@@ -3,11 +3,14 @@
 package com.example.searchapp.ui.fragment_gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.searchapp.R
 import com.example.searchapp.databinding.FragmentGalleryBinding
+import com.example.searchapp.utils.onQuerySubmit
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -27,9 +30,26 @@ class FragmentGallery : Fragment(R.layout.fragment_gallery) {
             recycerViewGallery.adapter = pixabayImageAdapter
         }
 
-        viewModel.photos.observe(viewLifecycleOwner) {
-            pagingData->
+        viewModel.photos.observe(viewLifecycleOwner) { pagingData ->
             pixabayImageAdapter.submitData(viewLifecycleOwner.lifecycle, pagingData)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_gallery_menu, menu)
+
+
+        val actionSearch = menu.findItem(R.id.action_search)
+        val searchView = actionSearch.actionView as androidx.appcompat.widget.SearchView
+
+
+        searchView.onQuerySubmit {
+            viewModel.searchQuery.value = it
+            searchView.clearFocus()
         }
     }
 }
