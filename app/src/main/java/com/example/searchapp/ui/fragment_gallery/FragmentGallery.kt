f79956905f22be421b9ed1675b003e1e1695412f
lubshad <lubshad4u4@gmail.com>
2021-11-23 06:38:25 +0530

    nav graph added

---
+++ app/src/main/java/com/example/searchapp/ui/fragment_gallery/FragmentGallery.kt
@@ -0,0 +1,9 @@
+package com.example.searchapp.ui.fragment_gallery
+
+import androidx.fragment.app.Fragment
+import com.example.searchapp.R
+
+
+class FragmentGallery : Fragment(R.layout.fragment_gallery)
+
+
